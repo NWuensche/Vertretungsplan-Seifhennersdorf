391ddd29ff6e2cb34a5ffc8af76fc220a28761bc
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-09-23 09:10:27 +0200

    Add RepPlan Class

---
+++ app/src/main/java/vertretunggut/app/niklas/vertretungsplan/RepPlan.java
@@ -0,0 +1,40 @@
+package vertretunggut.app.niklas.vertretungsplan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nwuensche on 23.09.16.
+ */
+public class RepPlan {
+    List<String> preview;
+    List<String> fullText;
+
+    public RepPlan() {
+        preview = new ArrayList<>();
+        fullText = new ArrayList<>();
+    }
+
+    public void add(String input) {
+        if(toLongForScreen(input)) {
+            preview.add(input.substring(0, 4) + "..");
+            fullText.add(input);
+        }
+        else {
+            preview.add(input);
+            fullText.add(input);
+        }
+    }
+
+    private boolean toLongForScreen(String input) {
+        return input.length() > 6;
+    }
+
+    public String getFullTextAt(int position) {
+        return fullText.get(position);
+    }
+
+    public List<String> getPreviewList(){
+        return preview;
+    }
+}
