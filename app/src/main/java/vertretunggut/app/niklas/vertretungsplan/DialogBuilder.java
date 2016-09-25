5e61949f1f50ee5310ba0a2799b236e7c6866ab1
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-09-25 22:08:29 +0200

    Add TODO

--- app/src/main/java/vertretunggut/app/niklas/vertretungsplan/DialogBuilder.java
+++ app/src/main/java/vertretunggut/app/niklas/vertretungsplan/DialogBuilder.java
@@ -3,6 +3,7 @@ package vertretunggut.app.niklas.vertretungsplan;
 /**
  * Created by nwuensche on 25.09.16.
  */
+// TODO wirklich Interface
 public interface DialogBuilder {
 
     public void buildDialog();
