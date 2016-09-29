bdcf6103845bc0dd7f76213584ee6db1272764dc
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-09-29 21:53:35 +0200

    Add new about page. Version 2.2

--- app/src/main/java/vertretunggut/app/niklas/vertretungsplan/MainActivity.java
+++ app/src/main/java/vertretunggut/app/niklas/vertretungsplan/MainActivity.java
@@ -1,5 +1,8 @@
 package vertretunggut.app.niklas.vertretungsplan;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +10,11 @@ import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.RelativeLayout;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
@@ -78,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
         switch(item.getItemId()){
             case R.id.About:
-                new AboutDialog(this).buildDialog();
+                createAboutPage();
                 break;
             case R.id.Exit:
                 System.exit(0);
@@ -86,6 +94,53 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createAboutPage(){
+        Element logo = getLicenseElement("dem Logo", "http://game-icons.net/");
+        Element jSoup = getLicenseElement("jsoup", "http://jsoup.org/");
+        Element aboutPageBuilder = getLicenseElement("android-about-page", "https://github.com/medyo/android-about-page");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false) // TODO ?
+                .setDescription("Dies ist die offizielle Vertretungsplan-App vom Oberland-Gymnasium Seifhennersdorf")
+                .setImage(R.drawable.buch)
+                .addGroup("Kontakt")
+                .addEmail("wuensche.niklas@gmail.com")
+                .addPlayStore("niklas.app.vertretunggut")
+                .addGitHub("nwuensche")
+                .addGroup("Lizenzen")
+                .addItem(logo)
+                .addItem(jSoup)
+                .addItem(aboutPageBuilder)
+                .create();
+
+        RelativeLayout aboutLayout = (RelativeLayout) findViewById(R.id.aboutLayout);
+        aboutLayout.addView(aboutPage);
+
+        FloatingActionButton prevDay = (FloatingActionButton) findViewById(R.id.prev_day_button);
+        FloatingActionButton nextDay = (FloatingActionButton) findViewById(R.id.next_day_button);
+        prevDay.hide();
+        nextDay.hide();
+        findViewById(R.id.list_of_reps).setVisibility(View.GONE);
+        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+        findViewById(R.id.noNetworkLayout).setVisibility(View.GONE);
+
+    }
+
+    public Element getLicenseElement(String name, final String website) {
+        Element item = new Element();
+
+        item.setTitle("Klick hier für die Website von " + name);
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+                startActivity(browserIntent);
+            }
+        });
+
+        return item;
     }
 
     public void restartRepPlanGetter() {
