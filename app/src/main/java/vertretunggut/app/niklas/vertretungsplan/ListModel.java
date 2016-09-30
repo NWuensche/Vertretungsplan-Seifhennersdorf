07f9739934434c8e5f091d32cd6cc3a6e0f910de
nwuensche <niklas.wuensche@mailbox.tu-dresden.de>
2016-09-30 08:43:16 +0200

    Add data set for list

---
+++ app/src/main/java/vertretunggut/app/niklas/vertretungsplan/ListModel.java
@@ -0,0 +1,44 @@
+package vertretunggut.app.niklas.vertretungsplan;
+
+/**
+ * Created by nwuensche on 30.09.16.
+ */
+public class ListModel {
+    private  String CompanyName="";
+    private  String Image="";
+    private  String Url="";
+
+    /*********** Set Methods ******************/
+
+    public void setCompanyName(String CompanyName)
+    {
+        this.CompanyName = CompanyName;
+    }
+
+    public void setImage(String Image)
+    {
+        this.Image = Image;
+    }
+
+    public void setUrl(String Url)
+    {
+        this.Url = Url;
+    }
+
+    /*********** Get Methods ****************/
+
+    public String getCompanyName()
+    {
+        return this.CompanyName;
+    }
+
+    public String getImage()
+    {
+        return this.Image;
+    }
+
+    public String getUrl()
+    {
+        return this.Url;
+    }
+}
