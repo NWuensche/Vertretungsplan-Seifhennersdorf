a7405928b3377f4e1ed1e14bdeadc37215bf0225
nwuensche <Wuensche.Niklas@gmail.com>
2019-05-11 15:25:46 +0200

    RepPlanLine to Kotlin

---
+++ app/src/main/java/vertretunggut/app/niklas/vertretungsplan/RepPlanLine.kt
@@ -0,0 +1,10 @@
+package vertretunggut.app.niklas.vertretungsplan
+
+/**
+ * Created by nwuensche on 30.09.16.
+ */
+data class RepPlanLine(val hour: String, val teacher: String, val subject: String, val room: String, val schoolClass: String, val type: String, val message: String) {
+
+    val isEmpty: Boolean
+        get() = hour == "" && teacher == "" && subject == "" && room == "" && schoolClass == "" && type == "" && message == ""
+}
